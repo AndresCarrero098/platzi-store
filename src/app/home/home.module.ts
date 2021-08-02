43827cccb2dae14e321d886cdc859becd7411451
Andres Carrero <andres_car098@hotmail.com>
2021-08-01 21:32:23 -0500

    Implementando lazy loading

---
+++ src/app/home/home.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+
+import { HomeRoutingModule } from './home-routing.module';
+import { BannerComponent } from './components/banner/banner.component';
+import { HomeComponent } from './home.component';
+
+@NgModule({
+  declarations: [BannerComponent, HomeComponent],
+  imports: [HomeRoutingModule],
+})
+export class HomeModule {}
