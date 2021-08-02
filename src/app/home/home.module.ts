9c5541f3367a0bc4850a80ebc0d20a491bd3db2f
Andres Carrero <andres_car098@hotmail.com>
2021-08-01 21:48:16 -0500

    Continuando lazy loading

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { BannerComponent } from './components/banner/banner.component';
-import { HomeComponent } from './home.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [BannerComponent, HomeComponent],
-  imports: [HomeRoutingModule],
+  imports: [CommonModule, HomeRoutingModule],
 })
 export class HomeModule {}
