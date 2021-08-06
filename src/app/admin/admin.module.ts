f66ff26891132a4f239494156167c90bbe93d67b
Andres Carrero <andres_car098@hotmail.com>
2021-08-06 17:34:00 -0500

    Creando componente de inventario

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
-import { LayoutModule } from '@angular/cdk/layout';
 
 import { AdminRoutingModule } from './admin-routing.module';
 import { MaterialModule } from '../material/material.module';
@@ -10,13 +9,13 @@ import { NavComponent } from './components/nav/nav.component';
 import { ProductFormComponent } from './components/product-form/product-form.component';
 import { TableComponent } from './components/table/table.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ProductsListComponent } from './components/products-list/products-list.component';
 
 @NgModule({
-  declarations: [NavComponent, ProductFormComponent, TableComponent, DashboardComponent],
+  declarations: [NavComponent, ProductFormComponent, TableComponent, DashboardComponent, ProductsListComponent],
   imports: [
     CommonModule,
     ReactiveFormsModule,
-    LayoutModule,
     AdminRoutingModule,
     MaterialModule,
   ],
