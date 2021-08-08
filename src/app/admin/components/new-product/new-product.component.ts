aa19b63ea4b68f358c8762e76a4fb352fd181d38
Andres Carrero <andres_car098@hotmail.com>
2021-08-08 16:22:37 -0500

    Ajustando formulario de productos

--- src/app/admin/components/new-product/new-product.component.ts
+++ src/app/admin/components/new-product/new-product.component.ts
@@ -1,22 +1,37 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
+import { ProductsService } from '../../../core/services/products/products.service';
 
 @Component({
   selector: 'app-new-product',
   templateUrl: './new-product.component.html',
-  styleUrls: ['./new-product.component.scss']
+  styleUrls: ['./new-product.component.scss'],
 })
 export class NewProductComponent implements OnInit {
 
   form: FormGroup;
 
   constructor(
-    private formBuilder: FormBuilder
+    private formBuilder: FormBuilder,
+    private productsService: ProductsService,
+    private router: Router
   ) {
     this.buildForm();
   }
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  saveProduct(event: Event) {
+    event.preventDefault();
+    if (this.form.valid) {
+      const product = this.form.value;
+      this.productsService.createProduct(product).subscribe(newProduct => {
+        console.log(newProduct);
+        this.router.navigate(['./admin/products'])
+      });
+    }
   }
 
   private buildForm() {
@@ -28,5 +43,4 @@ export class NewProductComponent implements OnInit {
       description: ['', [Validators.required]],
     });
   }
-
 }
