a5aa301bec409282a73d2e93dd9b5f5556e06350
Andres Carrero <andres_car098@hotmail.com>
2021-08-08 20:59:34 -0500

    Aplicando pipe a servicio de carrito

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,24 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
 import { CartService } from '../../../core/services/cart.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
+  
+  total$: Observable<number>;
 
-  total = 0;
-
-  constructor(private cartService: CartService) { 
-    this.cartService.cart$.subscribe(products => {
-      console.log(products);
-      this.total = products.length;
-    })
+  constructor(private cartService: CartService) {
+    this.total$ = this.cartService.cart$.pipe(
+      map((products) => products.length)
+    );
   }
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
